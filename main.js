af7e6b30fc3d31c67d5d5981218c5094d7cdd8c1
Eduarda Nere Rodrigues <eduardanere890@gmail.com>
2025-07-02 17:21:24 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+const alunos = [
+    { nome: "Ana", nota: 8 },
+    { nome: "João", nota: 5 },
+    { nome: "Maria", nota: 7 },
+    { nome: "Pedro", nota: 4 },
+    { nome: "Eduarda", nota: 9 },
+    { nome: "Daniel", nota: 6 }
+];
+
+function filtrarAprovados(listaDeAlunos) {
+    return listaDeAlunos.filter(aluno => aluno.nota >= 6);
+}
+
+console.log(filtrarAprovados(alunos));
+
+// no terminal executar o camando "node main.js"
